fe10f60eb171e6a849a0304f426fc4f9523f62b5
Karan <karansbisht7@gmail.com>
2024-11-05 22:38:45 +0530

    User Profile UI created

--- frontEnd/src/pages/Profile.jsx
+++ frontEnd/src/pages/Profile.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 
 function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-4xl mt-10 text-center font-bold">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          className="w-24 h-24 object-cover cursor-pointer self-center mt-4 rounded-full"
+          src={currentUser.avatar}
+          alt="profile image"
+        />
+        <input
+          type="text"
+          className="border p-3 max-w-xl rounded-lg"
+          placeholder="username"
+          name="username"
+          id="username"
+        />
+        <input
+          type="email"
+          className="border p-3 max-w-xl rounded-lg"
+          placeholder="email"
+          name="email"
+          id="email"
+        />
+        <input
+          type="text"
+          className="border p-3 max-w-xl rounded-lg"
+          placeholder="password"
+          name="password"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="mt-3 flex justify-between">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
     </div>
   );
 }
